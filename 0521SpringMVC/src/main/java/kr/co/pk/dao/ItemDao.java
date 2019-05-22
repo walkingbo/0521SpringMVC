4a58d06737f09ce33d8c03f994f5d4b250c60461
503 <a503_18@localhost>
2019-05-22 11:37:54 +0900

    0522

--- 0521SpringMVC/src/main/java/kr/co/pk/dao/ItemDao.java
+++ 0521SpringMVC/src/main/java/kr/co/pk/dao/ItemDao.java
@@ -3,8 +3,11 @@ package kr.co.pk.dao;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import kr.co.pk.domain.Item;
 
@@ -14,13 +17,24 @@ public class ItemDao {
 	@Autowired
 	private SqlSession sqlSession;
 	
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	
+	
 	//Item 테이블의 모든 데이터를 가져오는 메소드
+	@Transactional
 	public List<Item> allItem() {
-		return sqlSession.selectList("itemMapper.allItem");
+		//return sqlSession.selectList("itemMapper.allItem");
+		Session session = sessionFactory.getCurrentSession();
+		return session.createCriteria(Item.class).list();
 	}
 	
+	@Transactional
 	//itemid를 이용해서 item 테이블에서 데이터를 가져오는 메소드
 	public Item getItem(int itemid) {
-		return sqlSession.selectOne("itemMapper.getItem",itemid);
+		//return sqlSession.selectOne("itemMapper.getItem",itemid);
+		Session session = sessionFactory.getCurrentSession();
+		return session.get(Item.class, itemid);
 	}
 }
